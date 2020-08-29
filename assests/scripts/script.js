9154c46377ffc5f0495c282ce9bad6a759171aca
Wernher <wdhernandez@unal.edu.co>
2020-08-28 21:24:27 -0500

    280820_TallerFormularioMasCalculadora

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,83 +1,32 @@
+/* taller 27-08-20 */
+const mensajeNombre = document.getElementById('mensajeAlertaNombre')
+console.log(mensajeNombre)
 
-/* Ejemplo funciones 250820Función*/
+const mensajeAlertaTyC = document.getElementById('mensajeAlertaTyC')
+console.log(mensajeAlertaTyC)
 
-/* var divMensaje = document.getElementById('mensaje')
-console.log(divMensaje)
-console.log(divMensaje.innerText)
+function validarFormulario(e){
+    e.preventDefault()
+    let inputName = document.getElementById('name').value
+    console.log(inputName)
+    const inputTyC = document.getElementById('inputTyC')
+    console.log(inputTyC)
+    if(inputName == ''){
+    mensajeNombre.innerText = "Por favor llena el campo Nombre"
+    //inputName.classlist.add(BgcAlert)
+    swal("Error!", "Falta el campo Nombre!", "error");
+  
+    }
+    if(inputTyC.checked ==false){
+        mensajeAlertaTyC.innerText = 'Debes Aceptar Terminos'
+        swal("Error!", "Falta Aceptar terminos!", "error");
+        
+    }
 
- */
-function btnCantante (){
-    var Nombre = prompt('Escribe el nombre')
-    console.log("Nombre ", Nombre)
-    document.getElementById("1Nombre").innerHTML = Nombre; 
-    var edad = prompt('Escribe la Edad')
-    console.log("edad ", edad)
-    document.getElementById("1edad").innerHTML = edad; 
-    var profesion = prompt('Escribe la profesion')
-    console.log("profesion ", profesion)
-    document.getElementById("1profesion").innerHTML = profesion; 
-    var banda = prompt('Escribe la Banda')
-    console.log("Nombre ", banda)
-    document.getElementById("1banda").innerHTML = banda; 
+    if(inputName != "" && inputTyC.checked) {
+        swal("Muy Bien!", "Registro Exitoso!", "Success");
+        setTimeout(()=>{window.location = './assests/pages/calc.html'},2000)
+    }
 
-/*     divNombre.innerText  = Nombre
-    divEdad.innerText  = edad   
-    divProfesion.innerText  = profesion
-    divbanda.innerText  = banda */
-
-}
-
-
-/* function pedirApodo (){
-    var apodo = prompt('Escribe tu apodo')
-    console.log("Apodo ",apodo)
-}
-
-function darMensaje (){
-    var nombre = prompt('Escribe tu nombre')
-    var apodo = prompt('Escribe tu apodo')
-    var mensaje =`Mi nombre es ${nombre} pero mi apodo es ${apodo}`
-    console.log(mensaje)
-    divMensaje.innerText  = mensaje
     
-} */
-
-/* var btnApodo = document.querySelector('#btnApodo')
- console.log("este es el boton del apodo", btnApodo) 
-
-var btn3 = document.querySelector('.btn3') 
-console.log("Apodo2 btn3 ", btn3)*/
-
-
-/* Opción 1. Funcion Tipo Flecha*/
-
-/*  btnApodo.addEventListener('click', ()=>{
-var apodo = prompt('Escribe tu apodo')
-console.log(apodo)
-} )   */
-
-/* Opción 2. Funcion Tipo Flecha*/
-
-/* function pedirApodo (){
-    var apodo = prompt('Escribe tu apodo')
-    console.log(apodo)
 }
-
-btnApodo.addEventListener('click', pedirApodo )
- */
-
- /*
-// Forma 2
-var pedirApodo = () => {
- var apodo  = prompt('Escribe tu apodo')
-
-}
-
-// Forma 4
-
-btnApodo.addEventListener('click', ()=>{
-    var apodo  = prompt('Escribe tu apodo')
-}
-
-
-console.log("este es el boton del apodo", btnApodo) */
